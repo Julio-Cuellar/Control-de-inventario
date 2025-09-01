a54172eeef91740e86e5391d294368f3d0a45e86
Julio-Cuellar <anyaquimera1@gmail.com>
2025-09-01 02:20:52 -0600

    primer backup

---
+++ ContoldeInventario/dicoveryserver/src/main/java/com/JCode/dicoveryserver/DicoveryserverApplication.java
@@ -0,0 +1,13 @@
+package com.JCode.dicoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicoveryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicoveryserverApplication.class, args);
+	}
+
+}
